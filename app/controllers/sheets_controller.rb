201535376f8c6c24b13558084181e95bcb489af7
milfred <jasonmilfred@gmail.com>
2016-09-09 11:48:24 -0500

    Add sheets show view and controller

---
+++ app/controllers/sheets_controller.rb
@@ -0,0 +1,9 @@
+class SheetsController < ApplicationController
+  def show
+    @sheet = Sheet.find_by(url_parameter: params[:param])
+    @players = @sheet.ranked_players.first(300).sort do |a,b|
+      a.player_rank(@sheet) <=> b.player_rank(@sheet)
+    end
+    render "show"
+  end
+end
