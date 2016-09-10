c68c4a0d7269f2a5de5f760d61d443d88b50fc80
milfred <jasonmilfred@gmail.com>
2016-09-10 07:45:18 -0500

    Fix ranked players display order

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -2,15 +2,13 @@ class SheetsController < ApplicationController
 
   def show
     @sheet = Sheet.find_by(url_parameter: params[:param])
-    @players = @sheet.ranked_players.first(300).sort do |a,b|
-      a.player_rank(@sheet) <=> b.player_rank(@sheet)
-    end
+    @players = @sheet.ranked_players.sort {|a,b| a.player_rank(@sheet) <=> b.player_rank(@sheet)}.first(300)
     render "show"
   end
 
   def update
     if request.xhr
-      
+
     else
 
     end
