31ac5750cd89c63e1979019557baf88b0a93c423
milfred <jasonmilfred@gmail.com>
2016-10-01 09:24:19 -0500

    Add message if no results found in search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -7,9 +7,10 @@ $(function() {
   $(".search-field").on("keyup", function() {
     var searchField = $(this);
     var playerName = searchField.val();
-    var url = searchField.attr("action");
+    var url = $(".search-form").attr("action");
     var data = "search=" + playerName;
     var setting = $(".search-setting").val();
+    console.log(url);
 
     if(playerName.length >= 3) {
       var request = $.ajax({
@@ -19,14 +20,17 @@ $(function() {
       });
 
       request.done(function(response) {
-        console.log(response);
         var fadeDelay = 0;
         $("#search-results").html("");
 
-        $.each(response, function(index, player) {
-          $("<li><a href='#" + player.id + "'>" + player.name + " " + player.position + " " + player.team + "</a></li>").hide().appendTo("#search-results").delay(fadeDelay).fadeIn(100);
-          fadeDelay += 50;
-        });
+        if(response.length > 0) {
+          $.each(response, function(index, player) {
+            $("<li><a href='#" + player.id + "'>" + player.name + " " + player.position + " " + player.team + "</a></li>").hide().appendTo("#search-results").delay(fadeDelay).fadeIn(100);
+            fadeDelay += 50;
+          });
+        } else {
+          $("<li>No Results Found</li>").hide().appendTo("#search-results").fadeIn(100);
+        }
       });
     } else {
       $("#search-results").html("");
