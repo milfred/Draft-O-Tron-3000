03d017174222caac516c9203d398d475d4803b25
milfred <jasonmilfred@gmail.com>
2016-10-12 10:25:33 -0500

    Make another attempt to fix repetitive search results bugby changing event handler to accept keypress, change, and input

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -15,7 +15,7 @@ $(function() {
   });
 
 
-  $(".search-field").on("keyup", function() {
+  $(".search-field").on("keypress change input", function() {
     var searchField = $(this);
     var keyword = searchField.val();
     var url = $(".search-form").attr("action");
@@ -24,57 +24,70 @@ $(function() {
       var linkId = $(link).attr("href");
       return parseInt(linkId.substring(1, linkId.length));
     });
-      if(keyword.length >= 3) {
-        var request = $.ajax({
-          url: url,
-          method: "GET",
-          data: data
+    if(keyword.length >= 3) {
+      var request = $.ajax({
+        url: url,
+        method: "GET",
+        data: data
+      });
+
+      request.done(function(response) {
+
+        var newResults = $.map(response, function(index) {
+          return index.id;
         });
 
-        request.done(function(response) {
-
-          var newResults = $.map(response, function(index) {
-            return index.id;
-          });
-
-          var resultsToRemove = $.makeArray(currentResults).filter(function(val) {
-            return $.makeArray(newResults).indexOf(val) == -1;
-          });
-          var resultsToAdd = $.makeArray(newResults).filter(function(val) {
-            return $.makeArray(currentResults).indexOf(val) == -1;
-          });
-
-          if(response.length > 0) {
-            removeResults(resultsToRemove);
-            addResults(resultsToAdd, response);
-          } else {
-            $("#search-results").html("");
-            $("<li class='no-results white'>No results found</li>").hide().appendTo("#search-results").fadeIn(200);
-          }
+        var resultsToRemove = $.makeArray(currentResults).filter(function(val) {
+          return $.makeArray(newResults).indexOf(val) == -1;
         });
-      } else {
-        $("#search-results").html("");
-      }
+        var resultsToAdd = $.makeArray(newResults).filter(function(val) {
+          return $.makeArray(currentResults).indexOf(val) == -1;
+        });
+
+        if(response.length > 0) {
+          var fadeDelay = 0;
+          removeResults(resultsToRemove);
+          addResults(resultsToAdd, response);
+        } else {
+          $("#search-results").html("");
+          $("<li class='no-results white'>No results found</li>").hide().appendTo("#search-results").fadeIn(200);
+        }
+      });
+    } else {
+      $("#search-results").html("");
+    }
   });
 });
 
 
 function removeResults(removeArray) {
+  var fadeDelay = 0;
   if (removeArray.length > 0) {
     $.each(removeArray, function(index, playerId) {
-      $("#search-results a[href='#"  + playerId + "']").closest("li").fadeOut(100, function() {
+      $("#search-results a[href='#"  + playerId + "']").closest("li").delay(fadeDelay).fadeOut(100, function() {
         $(this).remove();
       });
+      fadeDelay += 50;
     });
   }
 }
 function addResults(addArray, response) {
+  var fadeDelay = 0;
   if (addArray.length > 0) {
     $(".no-results").remove();
     $.each(response, function(index, player) {
       if (addArray.includes(player.id)) {
-        $("<li><a class='white' href='#" + player.id + "'>" + player.name + " " + player.position + " " + player.team + "</a></li>").hide().appendTo("#search-results").fadeIn(100);
+        $("<li><a class='white' href='#" + player.id + "'>" + player.name + " " + player.position + " " + player.team + "</a></li>").hide().appendTo("#search-results").delay(fadeDelay).fadeIn(100);
+        fadeDelay += 50;
       }
     });
   }
 }
+
+var delay = (function(){
+  var timer = 0;
+  return function(callback, ms){
+    clearTimeout (timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
